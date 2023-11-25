5a4fcc637193161803d3f7a16ebbfb64a4a90add
Dylan <41908416+ErrorDodo@users.noreply.github.com>
2023-11-25 14:27:15 +0800

    Refactor handle_invite_create function

--- cnc_api/src/routes/invites/general.rs
+++ cnc_api/src/routes/invites/general.rs
@@ -1,5 +1,5 @@
-use crate::routes::invites::create_invite::build_invite;
-use actix_web::{HttpRequest, Responder};
-use log::info;
 
-pub async fn handle_invite_create(req: HttpRequest) {}
+use actix_web::{HttpRequest};
+
+
+pub async fn handle_invite_create(_req: HttpRequest) {}
